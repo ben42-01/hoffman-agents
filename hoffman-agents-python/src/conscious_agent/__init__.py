2402788ec6e677a5656754901ad4eb0e84e69dd3
Ben K <team.cryptoworx@gmail.com>
2026-06-28 13:25:46 +0100

    Bump python lib version to 2.0.1

--- hoffman-agents-python/src/conscious_agent/__init__.py
+++ hoffman-agents-python/src/conscious_agent/__init__.py
@@ -13,7 +13,7 @@ Quick start:
     print(f'"I" locked: {agent.experience.self_token.locked}')
 """
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 from .agent import ConsciousAgent, SimpleWorld, WorldState, StepOutput, ExperienceSpace
 from .world import World, WorldBuilder, SelfWorld, CoinTossWorld, build_world_from_dataframe
